9eb96ecbef6976011ed404d80efcc8ab1bcd2ff2
Daniel Parsley <danielparsley92@gmail.com>
2021-04-06 20:03:20 -0400

    commit tree 203a915198bc05c8223d503fd0b113f65d7605b8

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 =31, num2 = 2, num3 = 100, num4 = 24
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let mod = num3 % num4
+
+let max = Math.max(1, 2, 3, 20)
